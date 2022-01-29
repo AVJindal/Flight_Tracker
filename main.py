595c72f311ea2a4f4d49c2b8438d3fb604fa5bdc
AVJindal <avjindal@live.ca>
2022-01-29 11:55:37 -0400

    Edited Api

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from api import utils
+
 def main_wrapper():
     print(f"This is the start of our Python Project. This function's name is {main_wrapper.__name__}")
 
     # code here
-    #
+    utils.solid_example_1(example_param_2='r', example_param_2=5)
     print("This is the end of our Python Project.")
 
 
