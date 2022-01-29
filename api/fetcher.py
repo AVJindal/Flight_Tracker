01361941c74460139ec03650a99bd92adc12bb67
AVJindal <avjindal@live.ca>
2022-01-29 12:38:23 -0400

    Setting up the Fetcher

--- api/fetcher.py
+++ api/fetcher.py
@@ -3,6 +3,7 @@ from api.config import settings
 
 ROOT_URL = settings.ROOT_URL
 
+
 def states_accessor():
     # go through the doc api examples
     url = f"{ROOT_URL}/states/all"
@@ -11,8 +12,9 @@ def states_accessor():
         raise RuntimeError(r.json())
     print(r.json())
 
+
 def tracks_accessor():
-    url= f""
+    url = f""
     r = requests.get(url)
     if not r.ok:
         raise RuntimeError(r.json())
